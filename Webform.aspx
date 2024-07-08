a75feae00e78decbe3b6f80c4b0e2d9f4ac58f5a
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-08 11:27:04 +0300

    The project files

---
+++ Webform.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Webform.aspx.cs" Inherits="test5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ASP.NET project</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Firstname</td>
+                    <td><asp:TextBox ID="firstname" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>lastname</td>
+                    <td><asp:TextBox ID="lastname" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>city</td>
+                    <td><asp:TextBox ID="city" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>id (Only to update data!!)</td>
+                    <td><asp:TextBox ID="id" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click1" />
+                        <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click" />
+                        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click" />
+                        <asp:Button ID="Button4" runat="server" Text="View" OnClick="Button4_Click" />
+                    </td>
+                </tr>
+            </table>
+
+            <br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Webform.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace test5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SampleDatabase.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+         //   disp_data();
+
+        }
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Table1 values('" + firstname.Text + "', '" + lastname.Text + "', '" + city.Text + "')";
+            cmd.ExecuteNonQuery();
+            firstname.Text = "";
+            lastname.Text = "";
+            city.Text = "";
+            
+          //  disp_data();
+        }
+     
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Delete from Table1 where firstname='" + firstname.Text + "'";
+            cmd.ExecuteNonQuery();
+            firstname.Text = "";
+           // disp_data() ;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Update Table1 set firstname='"+firstname.Text+"',lastname='"+lastname.Text+"',city='"+city.Text+"' where id=" + Convert.ToInt32(id.Text) + "";
+            cmd.ExecuteNonQuery();
+            firstname.Text = "";
+            lastname.Text = "";
+            city.Text = "";
+            id.Text = "";
+           // disp_data();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Table1";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        //You can use it as a function to keep track of data
+
+        /*  public void disp_data()
+       {
+           SqlCommand cmd = con.CreateCommand();
+           cmd.CommandType = CommandType.Text;
+           cmd.CommandText = "select * from Table1";
+           cmd.ExecuteNonQuery();
+           DataTable dt = new DataTable();
+           SqlDataAdapter da = new SqlDataAdapter(cmd);
+           da.Fill(dt);
+           GridView1.DataSource = dt;
+           GridView1.DataBind();
+       } */
+    }
+}
